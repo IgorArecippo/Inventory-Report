52d4852aefe8632a03f8d8427c45e245260d0e65
Igor Arecippo <iarecippo@gmail.com>
2023-07-13 15:09:32 -0300

    feat: class Product creation tests

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product(
+        id="123",
+        product_name="Test Product",
+        company_name="Test Company",
+        manufacturing_date="13-07-2023",
+        expiration_date="13-07-2023",
+        serial_number="ABC123",
+        storage_instructions="Store in a cool place."
+    )
+
+    assert product.id == "123"
+    assert product.product_name == "Test Product"
+    assert product.company_name == "Test Company"
+    assert product.manufacturing_date == "13-07-2023"
+    assert product.expiration_date == "13-07-2023"
+    assert product.serial_number == "ABC123"
+    assert product.storage_instructions == "Store in a cool place."
