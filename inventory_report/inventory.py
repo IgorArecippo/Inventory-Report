fa317807659509ac799b67c5f31e803e69ad7f75
Igor Arecippo <iarecippo@gmail.com>
2023-07-19 13:58:35 -0300

    fix: inventory class return fixes

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,14 +1,14 @@
-from typing import List
+from typing import List, Optional
 from inventory_report.product import Product
 
 
 class Inventory:
-    def __init__(self, data: List[Product] = None):
+    def __init__(self, data: Optional[List[Product]] = None):
         self._data = data if data is not None else []
 
     @property
-    def data(self):
+    def data(self) -> Optional[List[Product]]:
         return self._data.copy()
 
-    def add_data(self, products: List[Product]):
-        self._data.extend(products)
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
