0c1ad1558e1c8780e7fa8f0f4b4d14015d2fa2f0
Igor Arecippo <iarecippo@gmail.com>
2023-07-19 13:55:29 -0300

    feat: implements class Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from typing import List
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    def __init__(self, data: List[Product] = None):
+        self._data = data if data is not None else []
+
+    @property
+    def data(self):
+        return self._data.copy()
+
+    def add_data(self, products: List[Product]):
+        self._data.extend(products)
